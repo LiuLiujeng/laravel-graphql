ee65f77324cbd997f3c4368dc7418d0483acd118
yachu <acercarol999@gmail.com>
2019-01-08 13:46:40 +0800

    Add GraphQL query: User

---
+++ app/GraphQL/Query/UsersQuery.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\GraphQL\Query;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Query;
+use App\User;
+
+class UsersQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'users'
+    ];
+
+    public function type()
+    {
+        return Type::listOf(GraphQL::type('User'));
+    }
+
+    public function args()
+    {
+        return [
+            'id' => ['name' => 'id', 'type' => Type::int()],
+            'name' => ['name' => 'name', 'type' => Type::string()],
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        if (isset($args['id'])) {
+            return User::where('id', $args['id'])->get();
+        } elseif (isset($args['name'])) {
+            return User::where('name', $args['name'])->get();
+        } else {
+            return User::all();
+        }
+    }
+}
